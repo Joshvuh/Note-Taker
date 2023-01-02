6747f29ab2fcaf2d64b927ec55d575e8187ba362
Josh Lemmond <joshualemmond@gmail.com>
2023-01-02 12:55:58 -0500

    confirmed funcionality

--- server.js
+++ server.js
@@ -10,23 +10,24 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Path for notes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(main, "notes.html"));
 });
-
+// API path for notes JSON file
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
-
+// API path for notes JSON file, specific ID within that file
 app.get("/api/notes/:id", function(req, res) {
     let storedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(storedNotes[Number(req.params.id)]);
 });
-
+// Default path if user inputs an invalid path
 app.get("*", function(req, res) {
     res.sendFile(path.join(main, "index.html"));
 });
-
+// Stores new notes to JSON file
 app.post("/api/notes", function(req, res) {
     let storedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
@@ -38,7 +39,7 @@ app.post("/api/notes", function(req, res) {
     console.log("Note saved to db.json. Content: ", newNote);
     res.json(storedNotes);
 })
-
+// Allows us to delete notes from JSON file through the user interface
 app.delete("/api/notes/:id", function(req, res) {
     let storedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let noteID = req.params.id;
@@ -56,7 +57,7 @@ app.delete("/api/notes/:id", function(req, res) {
     fs.writeFileSync("./db/db.json", JSON.stringify(storedNotes));
     res.json(storedNotes);
 })
-
+// Initialize app
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
